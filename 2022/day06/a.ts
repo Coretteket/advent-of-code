39df1fea5a4cc1607896c48f06d4e4538ead2554
Quinten Coret <Qcoret@gmail.com>
2022-12-06 13:07:05 +0100

    day 6

---
+++ 2022/day06/a.ts
@@ -0,0 +1,12 @@
+// Advent of Code 2022 - Day 6a
+
+const file = await Deno.readTextFile('input.txt');
+
+const getMarker = (size: number) => {
+  for (let i = size; i <= file.length; i++) {
+    const set = new Set(file.slice(i - size, i));
+    if (set.size === size) return i;
+  }
+};
+
+console.log(getMarker(4));
