284cee3ba805aca24ad3773a07c45d89e35e1d19
Quinten Coret <Qcoret@gmail.com>
2022-12-07 16:15:30 +0100

    cleanup

--- 2022/day07/a.ts
+++ 2022/day07/a.ts
@@ -3,20 +3,12 @@
 const file = await Deno.readTextFile('input.txt');
 const lines = file.split('\r\n');
 
-class File {
-  name: string;
-  size: number;
-
-  constructor(name: string, size: string | number) {
-    this.name = name;
-    this.size = Number(size);
-  }
-}
+type File = { name: string; size: number };
 
 class Dir {
   parent: Dir;
   name: string;
-  children: File[] = [];
+  files: File[] = [];
   dirs: Dir[] = [];
 
   constructor(parent: Dir, name: string) {
@@ -24,16 +16,8 @@ class Dir {
     this.name = name;
   }
 
-  addKid(name: string, size: string) {
-    this.children.push(new File(name, size));
-  }
-
-  addDir(dir: Dir) {
-    this.dirs.push(dir);
-  }
-
   get size(): number {
-    return [...this.children, ...this.dirs].reduce((prev, curr) => prev + curr.size, 0);
+    return [...this.files, ...this.dirs].reduce((prev, curr) => prev + curr.size, 0);
   }
 }
 
@@ -45,21 +29,17 @@ lines.slice(1).forEach((line) => {
       dir = dir.parent;
     } else {
       const new_dir = new Dir(dir, line.replace('$ cd ', ''));
-      dir.addDir(new_dir);
+      dir.dirs.push(new_dir);
       dir = new_dir;
     }
   }
   if (!line.match(/(dir|\$)/)) {
-    const [number, name] = line.split(' ');
-    dir.addKid(name, number);
+    const [size, name] = line.split(' ');
+    dir.files.push({ name, size: Number(size) });
   }
 });
 
-const getRoot = (dir: Dir) => {
-  if (dir.name === '/' && dir.parent === null) return dir;
-  return dir.parent;
-};
-
+const getRoot = (dir: Dir) => (dir.name === '/' ? dir : dir.parent);
 const root = getRoot(dir);
 
 const list = (root: Dir) => {
--- 2022/day07/b.ts
+++ 2022/day07/b.ts
@@ -3,20 +3,12 @@
 const file = await Deno.readTextFile('input.txt');
 const lines = file.split('\r\n');
 
-class File {
-  name: string;
-  size: number;
-
-  constructor(name: string, size: string | number) {
-    this.name = name;
-    this.size = Number(size);
-  }
-}
+type File = { name: string; size: number };
 
 class Dir {
   parent: Dir;
   name: string;
-  children: File[] = [];
+  files: File[] = [];
   dirs: Dir[] = [];
 
   constructor(parent: Dir, name: string) {
@@ -24,16 +16,8 @@ class Dir {
     this.name = name;
   }
 
-  addKid(name: string, size: string) {
-    this.children.push(new File(name, size));
-  }
-
-  addDir(dir: Dir) {
-    this.dirs.push(dir);
-  }
-
   get size(): number {
-    return [...this.children, ...this.dirs].reduce((prev, curr) => prev + curr.size, 0);
+    return [...this.files, ...this.dirs].reduce((prev, curr) => prev + curr.size, 0);
   }
 }
 
@@ -45,24 +29,17 @@ lines.slice(1).forEach((line) => {
       dir = dir.parent;
     } else {
       const new_dir = new Dir(dir, line.replace('$ cd ', ''));
-      dir.addDir(new_dir);
+      dir.dirs.push(new_dir);
       dir = new_dir;
     }
   }
   if (!line.match(/(dir|\$)/)) {
-    const [number, name] = line.split(' ');
-    dir.addKid(name, number);
+    const [size, name] = line.split(' ');
+    dir.files.push({ name, size: Number(size) });
   }
 });
 
-const total_size = 70_000_000;
-const min_unused = 30_000_000;
-
-const getRoot = (dir: Dir) => {
-  if (dir.name === '/' && dir.parent === null) return dir;
-  return dir.parent;
-};
-
+const getRoot = (dir: Dir) => (dir.name === '/' ? dir : dir.parent);
 const root = getRoot(dir);
 
 const list = (root: Dir) => {
@@ -72,7 +49,7 @@ const list = (root: Dir) => {
 };
 
 const output = list(root)
-  .filter((dir) => dir.size >= min_unused + root.size - total_size)
+  .filter((dir) => dir.size >= root.size - 4e7)
   .sort((a, b) => a.size - b.size)[0].size;
 
 console.log(output);
